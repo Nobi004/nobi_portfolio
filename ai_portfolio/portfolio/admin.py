d287ed1bb3caeacc655c03e3606500ec7718e4db
Mahmudun Nobi <mdmnb435@gmail.com>
2025-06-15 03:44:38 +0600

    Update admin.py

--- ai_portfolio/portfolio/admin.py
+++ ai_portfolio/portfolio/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from .models import (
+    Profile, SkillCategory, Skill, Experience, 
+    ProjectTag, Project, Education, Certification, 
+    BlogPost, ContactMessage
+)
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['name', 'title', 'email']
+
+@admin.register(SkillCategory)
+class SkillCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'order']
+    list_editable = ['order']
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'level', 'order']
+    list_filter = ['category']
+    list_editable = ['level', 'order']
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ['job_title', 'company_name', 'start_date', 'is_current']
+    list_filter = ['is_current']
+
+@admin.register(ProjectTag)
+class ProjectTagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'featured', 'order']
+    list_filter = ['featured', 'tags']
+    list_editable = ['featured', 'order']
+    filter_horizontal = ['tags']
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ['degree', 'institution', 'start_year']
+
+@admin.register(Certification)
+class CertificationAdmin(admin.ModelAdmin):
+    list_display = ['title', 'issuing_organization', 'issue_date']
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publication_platform', 'publication_date']
+    list_filter = ['publication_platform', 'tags']
+    filter_horizontal = ['tags']
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'subject', 'created_at', 'is_read']
+    list_filter = ['is_read', 'created_at']
+    readonly_fields = ['name', 'email', 'subject', 'message', 'created_at']
+    
+    def has_add_permission(self, request):
+        return False
