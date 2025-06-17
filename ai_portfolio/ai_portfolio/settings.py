f1bedd3c737019ea7116760fd091b63435b0816e
Mahmudun Nobi <mdmnb435@gmail.com>
2025-06-18 00:34:43 +0600

    Update settings.py

--- ai_portfolio/ai_portfolio/settings.py
+++ ai_portfolio/ai_portfolio/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = []
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
