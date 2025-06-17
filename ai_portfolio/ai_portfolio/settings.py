c764b6312a2262450d1c6d42e7dba8d9a2096cdf
Nobi004 <148976917+Nobi004@users.noreply.github.com>
2025-06-17 10:32:58 +0600

    Update settings.py

--- ai_portfolio/ai_portfolio/settings.py
+++ ai_portfolio/ai_portfolio/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = ['nobi-44sr.onrender.com']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
 
 
 
